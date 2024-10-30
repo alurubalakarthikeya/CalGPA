c789e27deaa64d1b16ef3126e34197b3f8fcca15
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2024-10-30 13:33:53 +0530

    meh

--- functional.js
+++ functional.js
@@ -7,3 +7,9 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function darkMode(){
+    if(document.body.style.backgroundColor == "white" || document.body.style.backgroundColor == "#fff" || document.body.style.backgroundColor == "--bg-color"){ 
+      document.body.style.backgroundColor = "#000000";
+    }
+  }
