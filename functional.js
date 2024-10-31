f922306ffa0fc6de8d44676bf673a32f2f7368f2
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2024-10-31 16:53:23 +0530

    dark-mode-added

--- functional.js
+++ functional.js
@@ -8,11 +8,9 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  function darkMode(){
-    if(document.body.style.backgroundColor == "white" || document.body.style.backgroundColor == "#fff" || document.body.style.backgroundColor == "--bg-color"){ 
-      document.body.style.backgroundColor = "#000000";
-    }
-  }
+  function darkMode() {
+    document.body.classList.toggle('dark-mode');
+}
 
   document.getElementById('button1').addEventListener('mouseover', function() {
     document.getElementById('button2').classList.add('hovered');
