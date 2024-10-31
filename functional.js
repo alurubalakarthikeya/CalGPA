b72c86b2a65e3708f3f4f72d733b6b54274567c1
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2024-10-31 16:07:53 +0530

    better-ui

--- functional.js
+++ functional.js
@@ -13,3 +13,19 @@ function openNav() {
       document.body.style.backgroundColor = "#000000";
     }
   }
+
+  document.getElementById('button1').addEventListener('mouseover', function() {
+    document.getElementById('button2').classList.add('hovered');
+});
+
+document.getElementById('button1').addEventListener('mouseout', function() {
+    document.getElementById('button2').classList.remove('hovered');
+});
+
+document.getElementById('button2').addEventListener('mouseover', function() {
+    document.getElementById('button1').classList.add('hovered');
+});
+
+document.getElementById('button2').addEventListener('mouseout', function() {
+    document.getElementById('button1').classList.remove('hovered');
+});
