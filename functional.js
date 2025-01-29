e6e43b86bde6fc523ac88f3167e7d27c040840ff
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2025-01-29 13:54:02 +0530

    pwa-trail1

--- functional.js
+++ functional.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
